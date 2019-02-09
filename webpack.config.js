fd37a6cbff7ed5d3270111e1fe8464870a683208
Steve Duong <steve@Plan.D>
2019-02-08 16:26:04 -0800

    adds eslint with AirBnb

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './client/src/index.jsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './client/dist')
+    path: path.resolve(__dirname, './client/dist'),
   },
   module: {
     rules: [
@@ -14,10 +14,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
 };
