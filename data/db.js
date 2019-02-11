33d8499313e596b359f2540bf1504cf2869f5ec1
Steve Duong <steve@Plan.D>
2019-02-11 15:38:51 -0800

    adds db functions to handle server queries

---
+++ data/db.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Password1!',
+  database: 'agents',
+});
+
+connection.connect();
+
+const getListedAgent = (houseId, callback) => {
+  connection.query(`SELECT * FROM listedAgent WHERE houseId = ${houseId}`, (err, data) => {
+    if (err) {
+      console.log('RECEIVING LIST AGENT FAILED', err);
+    } else {
+      callback(data);
+    }
+  });
+};
+
+const getPremierAgents = (houseId, callback) => {
+  connection.query('SELECT * FROM premierAgents', (err, data) => {
+    if (err) {
+      console.log('RETREVING PREMIER AGENTS FAILED', err);
+    } else {
+      callback(data);
+    }
+  });
+};
+
+module.exports = { getListedAgent, getPremierAgents };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const db = require('./db.js');
 const app = express();
 const PORT = 6969;
 
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(bodyParser.json());
+
+app.get('/houseId/listedAgent', (req, res) => {
+
+  res.send('listed req received');
+});
+
+app.get('/houseId/premierAgents', (req, res) => {
+  res.send('premier req received');
+});
 
 app.listen(PORT, () => {
   console.log(`Hidey-Ho Cap'n, we are now serving on port ${PORT}!`);
