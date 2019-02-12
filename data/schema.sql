8e136452e9681a624aa13e345dc9b51b4a3d1c9c
Steve Duong <steve@Plan.D>
2019-02-11 17:09:57 -0800

    adds database schema

---
+++ data/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS agents;
+CREATE DATABASE IF NOT EXISTS agents;
+USE agents;
+
+DROP TABLE IF EXISTS listedAgent, premierAgents;
+
+CREATE TABLE listedAgent(
+  id INT NOT NULL AUTO_INCREMENT,
+  house_id INT,
+  agent_name VARCHAR(100),
+  agent_company VARCHAR(100),
+  agent_phone INT,
+  agent_url VARCHAR(255),
+  PRIMARY KEY (id),
+  FOREIGN KEY (house_id),
+)
+
+CREATE TABLE premierAgent(
+  id INT NOT NULL AUTO_INCREMENT,
+  agent_name VARCHAR(100),
+  reviews INT,
+  recent_sales INT,
+  agent_phone INT,
+  agent_url VARCHAR(255),
+  PRIMARY KEY (id),
+)
