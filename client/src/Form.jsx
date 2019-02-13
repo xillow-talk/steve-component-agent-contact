2f0f9f231ee403bba8d52147edee7fd9795cddbf
Steve Duong <steve@Plan.D>
2019-02-13 15:17:21 -0800

    adds requested changes to Form component

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -3,20 +3,36 @@ import React from 'react';
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = {
+      information: {}
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+    const target = event.target;
+    var value = target.value;
+    const name = target.name;
+    const information = this.state.information;
+    information[name] = value;
+    this.setState({ information: information });
+  };
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
-  }
+    var data = {
+      name: this.state.information.name,
+      number: this.state.information.number,
+      email: this.state.information.email,
+      message: this.state.information.message,
+    }
+    alert('data collected and submitted: ' + data.name);
+    //post request here:
+    //saveClientInfo(data);
+  };
+
   render() {
     return (
       <div>
