109a87f5473c85f148a9f173b8322e56d7fd2161
Steve Duong <steve@Plan.D>
2019-02-15 08:45:53 -0800

    adds correct styling to form.

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
+//CSS
+const FormContainer = styled.div`
+  box-sizing: border-box;
+  display: block;
+  font-family: Gotham,gotham,Verdana,sans-serif;
+  line-height: 1.5;
+  color: #444;
+`;
+
+const Input = styled.input`
+  padding-left: 25px;
+  min-height: 30px;
+  font-size: .86666667rem;
+  height: 30px;
+  line-height: 1.5em;
+  width: 100%;
+  padding: 4px 6px 2px;
+  border: 1px solid #aaa;
+  background-color: #fff;
+  box-shadow: inset 0 2px 2px #fafafa, 0 0 0 #62aef7;
+  box-sizing: border-box;
+  color: inherit;
+  font: inherit;
+  margin: 0;
+
+  text-rendering: auto;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+`;
+
+//Component
 class Form extends React.Component {
   constructor(props) {
     super(props);
@@ -36,23 +73,23 @@ class Form extends React.Component {
 
   render() {
     return (
-      <div>
+      <FormContainer>
         <form onSubmit={this.handleSubmit}>
           <label>
-            <input type="text" name="name" placeholder="Your Name" value={this.state.value} onChange={this.handleChange} />
+            <Input type="text" name="name" placeholder="Your Name" value={this.state.value} onChange={this.handleChange} />
           </label>
           <label>
-            <input type="text" name="number" placeholder="Phone" value={this.state.value} onChange={this.handleChange} />
+            <Input type="text" name="number" placeholder="Phone" value={this.state.value} onChange={this.handleChange} />
           </label>
           <label>
-            <input type="text" name="email" placeholder="Email" value={this.state.value} onChange={this.handleChange} />
+            <Input type="text" name="email" placeholder="Email" value={this.state.value} onChange={this.handleChange} />
           </label>
           <label>
-            <input type="text" name="message" value={this.state.value} onChange={this.handleChange} placeholder="I am interested in 6760 Mission St, Daly City, CA 94014." />
+            <Input type="text" name="message" value={this.state.value} onChange={this.handleChange} placeholder="I am interested in 6760 Mission St, Daly City, CA 94014." />
           </label>
           <input type="submit" value="Contact Agent" />
         </form>
-      </div>
+      </FormContainer>
     )
   }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,21 +4,35 @@ import Form from './Form.jsx';
 import AllAgents from './AllAgents.jsx';
 import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+//CSS
+const Title = styled.h2`
+  display: block;
+  font-family: Gotham,gotham,Verdana,sans-serif;
+  font-size: 13px;
+  line-height: 1.5;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 10px;
 `;
 
+const AppContainer = styled.div`
+display: block;
+border: 1px solid #ccc;
+border-top: 2px solid #0074e4;
+box-shadow: 0 0 2px rgba(0,0,0,.2);
+padding: 10px;
+  `;
+
+//Component
 function App(props) {
   return (
-    <div>
+    <AppContainer>
       <Title>
         Contact Agent
       </Title>
       <Form />
       <AllAgents houseId={props.houseId} />
-    </div>
+    </AppContainer>
   )
 }
 
