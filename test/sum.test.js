b8606e58999269703a4752a0a6907c801ce1f979
Steve Duong <steve@Plan.D>
2019-02-18 22:47:09 -0800

    current progress on jest enzyme testing, not functional

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-
+import { shallow, mount, render } from 'enzyme';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 import App from '../client/src/index.jsx';
 
 
-// const sum = require('./sum');
 
-describe('<App />', () => {
-  it('should shallow', () => {
-    const Wrapper = mount(<App />)
-    expect(Wrapper).toMatchSnapshot()
-  })
-})
+// const sum = require('./sum');
+test('Should successfully mount App', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
+});
+
+// describe('<App />', () => {
+//   it('render correctly ', () => {
+//     const AppComponent = renderer.create(<App />).toJSON();
+//     expect(AppComponent).toMatchSnapshot();
+//   });
+//   it('should shallow', () => {
+//     const Wrapper = mount(<App />)
+//     expect(Wrapper).toMatchSnapshot()
+//   })
+// })
 
 // it('adds 1 + 2 = 3', () => {
 //   expect(sum(1, 2)).toBe(3);
